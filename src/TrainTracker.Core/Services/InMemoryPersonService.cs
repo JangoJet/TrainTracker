01cfb58b1641f5ccc2ee22a43d484770bc00c05f
Geoff Norton <Geoff.Norton@wales.nhs.uk>
2019-11-29 16:39:23 +0000

    Added a third data item

--- src/TrainTracker.Core/Services/InMemoryPersonService.cs
+++ src/TrainTracker.Core/Services/InMemoryPersonService.cs
@@ -10,8 +10,9 @@ namespace TrainTracker.Core.Services
         public IEnumerable<Person> GetPeople()
         {
             IEnumerable<Person> people = new List<Person>() { new Person() {Id=1, FirstName= "Geoff",  LastName="Norton"},
-                                                                                         new Person() {Id=2, FirstName= "Alice",  LastName="Norton"}
-                                                                                       };
+                                                                                         new Person() {Id=2, FirstName= "Alice",  LastName="Norton"},
+                                                                                         new Person(){Id=3, FirstName="Elinor",LastName="Norton"}
+            };
 
             return people;
         }
