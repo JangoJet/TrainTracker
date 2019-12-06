96c953e50dcdbfefc0d438a54a3081b139ace848
Geoff Norton <Geoff.Norton@wales.nhs.uk>
2019-12-06 16:45:31 +0000

    Added a fourth In-Memory Person item

--- src/TrainTracker.Core/Services/InMemoryPersonService.cs
+++ src/TrainTracker.Core/Services/InMemoryPersonService.cs
@@ -11,7 +11,8 @@ namespace TrainTracker.Core.Services
         {
             IEnumerable<Person> people = new List<Person>() { new Person() {Id=1, FirstName= "Geoff",  LastName="Norton"},
                                                                                          new Person() {Id=2, FirstName= "Alice",  LastName="Norton"},
-                                                                                         new Person(){Id=3, FirstName="Elinor",LastName="Norton"}
+                                                                                         new Person(){Id=3, FirstName="Elinor",LastName="Norton"},
+                                                                                         new Person(){Id=4,FirstName="Sandra", LastName="Norton"}
             };
 
             return people;
