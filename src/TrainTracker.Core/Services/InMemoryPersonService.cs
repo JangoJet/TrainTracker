5139b56f8d513de832b3d970f5af4cea3b8ed602
Geoff Norton <Geoff.Norton@wales.nhs.uk>
2020-01-22 09:49:19 +0000

    Added CRUD functionality to InMemoryServiceLayer #36

--- src/TrainTracker.Core/Services/InMemoryPersonService.cs
+++ src/TrainTracker.Core/Services/InMemoryPersonService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using TrainTracker.Core.Entities;
 
@@ -7,15 +8,39 @@ namespace TrainTracker.Core.Services
 {
     public class InMemoryPersonService : IPersonService
     {
-        public IEnumerable<Person> GetPeople()
-        {
-            IEnumerable<Person> people = new List<Person>() { new Person() {Id=1, FirstName= "Geoff",  LastName="Norton"},
-                                                                                         new Person() {Id=2, FirstName= "Alice",  LastName="Norton"},
-                                                                                         new Person(){Id=3, FirstName="Elinor",LastName="Norton"},
-                                                                                         new Person(){Id=4,FirstName="Sandra", LastName="Norton"}
-            };
+        private readonly List<Person> _people;
 
-            return people;
+        public InMemoryPersonService()
+        {
+            _people = new List<Person>() { new Person() {Id=1, FirstName= "Geoff",  LastName="Norton"}};
+        
+        }
+
+        public IEnumerable<Person> GetAll()
+        {
+            return _people;
+        }
+        public void Create(Person person)
+        {
+            _people.Add(person);
+        }
+
+        public Person FindById(int Id)
+        {
+            return _people.Where(p => p.Id == Id).FirstOrDefault();
+        }
+
+        public void Remove(int id, Person person)
+        {
+            _people.Remove(person);
+        }
+
+        public void Update(Person person)
+        {
+            var personToUpdate = FindById(person.Id);
+            personToUpdate.FirstName = person.FirstName;
+            personToUpdate.LastName = person.LastName;
+            personToUpdate.Email = person.Email;
         }
     }
 }
